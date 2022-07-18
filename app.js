78de5fde5741b23e28f477907f175138e9e621f2
Anton Kuzmenko <example@gmail.com>
2022-07-18 10:10:11 +0300

    app.js cinema.mod

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-const hey = prompt('Are you ready?');
+'use strict';
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actor: {},
+  genres: [],
+  privat: false,
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+  b = prompt('На сколько вы его оцениваете?'),
+  cinema = prompt('Один из последних просмотренных фильмов?', ''),
+  d = prompt('На сколько вы его оцениваете?');
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[cinema] = d;
+console.log(personalMovieDB);
